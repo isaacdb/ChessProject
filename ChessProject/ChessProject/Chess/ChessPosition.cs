99b1e8e32af8be5a3739d8e5ff3ce2d5c9f424c8
Isaac Debiasi <isaacdbs@outlook.com>
2020-11-05 11:39:32 -0300

    Adionando a classe de posicao xadrez, e convertendo os valores da matriz pra valores do xadrez

---
+++ ChessProject/ChessProject/Chess/ChessPosition.cs
@@ -0,0 +1,24 @@
+﻿using ChessProject.Board;
+
+namespace ChessProject.Chess
+{
+    class ChessPosition
+    {
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+        public ChessPosition(char column, int line)
+        {
+            Column = column;
+            Line = line;
+        }
+        public Position toPosition()
+        {
+            return new Position(8 - Line, Column - 'a');
+        }
+        public override string ToString()
+        {
+            return "" + Line + Column;
+        }
+    }
+}
