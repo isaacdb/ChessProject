60097b301ce087b87ed01366e5d38d0701c90f15
Isaac Debiasi <isaacdbs@outlook.com>
2020-11-05 11:32:02 -0300

    Tratanto alguns erros com excecoes personalizadas

--- ChessProject/ChessProject/Board/ChessBoard.cs
+++ ChessProject/ChessProject/Board/ChessBoard.cs
@@ -16,10 +16,34 @@
         {
             return Pieces[line, column];
         }
+        public Piece PiecePosition(Position pos)
+        {
+            return Pieces[pos.Line, pos.Column];
+        }
+        public bool ExistPiece(Position pos)
+        {
+            ValidadePosition(pos);
+            return PiecePosition(pos) != null;
+        }
         public void InsertPiece(Piece piec, Position pos)
         {
+            if (ExistPiece(pos))
+                throw new BoardException("Piece exist on this position");
             Pieces[pos.Line, pos.Column] = piec;
             piec.Position = pos;
+        }
+
+        public bool ValidPosition(Position pos)
+        {
+            if (pos.Line < 0 || pos.Line >= Lines || pos.Column < 0 || pos.Column >= Columns)
+                return false;
+            else
+                return true;
+        }
+         public void ValidadePosition(Position pos)
+        {
+            if (!ValidPosition(pos))
+                throw new BoardException("invalid Positon!");
         }
     }
 }
