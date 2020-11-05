d590709d0e99f44f1b03ca62f61651cca4918cac
Isaac Debiasi <isaacdbs@outlook.com>
2020-11-05 11:49:51 -0300

    Adionada difereca de cores entres as pecas

--- ChessProject/ChessProject/Program.cs
+++ ChessProject/ChessProject/Program.cs
@@ -12,9 +12,9 @@ namespace ChessProject
             ChessBoard chessBoard = new ChessBoard(8, 8);
 
             chessBoard.InsertPiece(new King(chessBoard, Color.Preta), new Position(1, 3));
-            chessBoard.InsertPiece(new Tower(chessBoard, Color.Preta), new Position(0, 3));
+            chessBoard.InsertPiece(new Tower(chessBoard, Color.Branca), new Position(0, 3));
             chessBoard.InsertPiece(new King(chessBoard, Color.Preta), new Position(7, 7));
-            chessBoard.InsertPiece(new Tower(chessBoard, Color.Preta), new Position(1, 3));
+            chessBoard.InsertPiece(new Tower(chessBoard, Color.Branca), new Position(1, 6));
 
             Screen.BoardImpress(chessBoard);
         }
--- ChessProject/ChessProject/Screen.cs
+++ ChessProject/ChessProject/Screen.cs
@@ -9,14 +9,31 @@ namespace ChessProject
         {
             for (int i = 0; i < chessBoard.Lines; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < chessBoard.Columns; j++)
                 {
                     if (chessBoard.PiecePositon(i, j) == null)
                         Console.Write("- ");
                     else
-                        Console.Write(chessBoard.PiecePositon(i, j) + " ");
+                    {
+                        PieceImpress(chessBoard.PiecePositon(i, j));
+                        Console.Write(" ");
+                    }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void PieceImpress(Piece piec)
+        {
+            if (piec.Color == Color.Branca)
+                Console.Write(piec);
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(piec);
+                Console.ForegroundColor = aux;
             }
         }
     }
